b5df7f2d54744c4af950150c48232517327a673a
SerhiyKonovalov <serhiykonovalov2003@gmail.com>
2023-05-16 00:09:09 +0300

    Реалізував дизайн та адаптацію завантаження фото в панелі додавання авто

--- car-for-subscriptions/js/add-panel.js
+++ car-for-subscriptions/js/add-panel.js
@@ -1,6 +1,3 @@
-
-
-
 async function addCar(){
     let newCar = {
         "name": document.getElementById('name').value,
@@ -17,24 +14,55 @@ async function addCar(){
         "lastServiceDate":document.getElementById('lastServiceDate').value,
         "statusId": "1",
         "image":document.getElementById('image').value 
-     
     };
     
 
     const response = await fetch('https://circular-ally-383113.lm.r.appspot.com/api/v1/cars', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(newCar)
-			});
-			let responseJSON = await response.json();
-			let status = responseJSON['status'];
-            if (status === 'success') {
-                let message = responseJSON['message'];
-				alert(message);
-			}else{
-				let error = responseJSON['errorMessage'];
-				alert(error);
-			}
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(newCar)
+	});
+	let responseJSON = await response.json();
+	let status = responseJSON['status'];
+    if (status === 'success') {
+        let message = responseJSON['message'];
+		alert(message);
+	}else{
+		let error = responseJSON['errorMessage'];
+		alert(error);
+	}
+}
+
+// получаю input file в змінну
+const image = document.getElementById('image');
+// получаю div для превью в змінну
+const preview = document.getElementById('preview');
+
+image.addEventListener('change', () => {
+    uploadFile(image.files[0]);
+});
+function uploadFile(file) {
+    // перевірка на тип файлу
+    if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+        alert('Дозволені тільки зображення!');
+        image.value = '';
+        return;
+    }
+    // перевірка на розмір файлу
+    if (file.size > 2 * 1024 * 1024) {
+        alert('Файл повинен бути меньше 2 Мб!');
+        return;
+    }
+
+    // конструкція FileReader
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        preview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+    };
+    reader.onerror = function (e) {
+        alert("Помилка");
+    };
+    reader.readAsDataURL(file);
 }
